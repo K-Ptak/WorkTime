d8c4cf05c43f4d2da498ba5828e8a97b607e87e9
K-Ptak <kamil2001p@gmail.com>
2023-02-01 14:04:15 +0100

    Add sound support and make the program loop itself

--- worktime.sh
+++ worktime.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+end () {
+    zenity --notification --text "END OF WORK!" & disown
+    if [ -f /usr/share/sounds/freedesktop/stereo/complete.oga ]; then
+        paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+    fi
+    killall yad
+    kill $$
+}
+
+sound () {
+    if [ -f /usr/share/sounds/freedesktop/stereo/message.oga ]; then
+        paplay /usr/share/sounds/freedesktop/stereo/message.oga
+    fi
+}
+
 
 config=".config"
 settings=()
@@ -15,22 +30,33 @@ lbtime=${settings[2]}
 pseq=${settings[3]}
 rtime=${settings[4]}
 ticon=${settings[5]}
+arg1=${1:-6}
 
 lastseq=$((pseq-1))
-echo $$
+
+
 if [[ "$ticon" == "true" ]]; then
     yad --notification --image="clock" --command="" --menu="Stop WorkTime ! kill -SIGTERM $$ yad" --text="WorkTime" & disown
 fi
 
-for ((n=0;n<"$pseq";n++))
+while :
 do
-    zenity --notification --text "Work time!" && sleep $wtime
-    if [[ "$rtime" == "true" ]]; then
-        zenity --notification --text "Review time!" && sleep 1m
-    fi
+    for ((n=0;n<"$pseq";n++))
+    do
+        zenity --notification --text "Work time!" & sound & sleep $wtime
+        arg1=$((arg1-1))
+        if [[ "$arg1" == "0" ]]; then
+            end
+        fi
 
-    if [[ "$n" != "$lastseq" ]]; then
-       zenity --notification --text "Break time!" && sleep $btime
-    fi
+
+        if [[ "$rtime" == "true" ]]; then
+            zenity --notification --text "Review time!" & sound & sleep 1m
+        fi
+
+        if [[ "$n" != "$lastseq" ]]; then
+        zenity --notification --text "Break time!" & sound & sleep $btime
+        fi
+    done
+    zenity --notification --text "Long break time!" & sound & sleep $lbtime
 done
-zenity --notification --text "Long break time!" && sleep $lbtime
